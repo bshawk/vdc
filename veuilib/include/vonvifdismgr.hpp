8389dda13968015f5afe2d81b40e97c753899332
xsmart <xsmart@163.com>
2014-08-16 18:07:34 +0800

    add onvif discover manager

---
+++ veuilib/include/vonvifdismgr.hpp
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// File: vonvifdismgr.hpp
+//
+// Desc: onvif discover manager - Manage ONVIF Discover.
+//
+// Copyright (c) 2014-2018 vdceye. All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef __V_ONVIF_DIS_MGR_HPP__
+#define __V_ONVIF_DIS_MGR_HPP__
+#include "utility.hpp"
+#include "debug.hpp"
+#include "videotype.hpp"
+#include <QWidget>
+#include "factory.hpp"
+#include <QThread>
+
+using namespace UtilityLib;
+
+class VE_LIBRARY_API VOnvifDisMgr
+{
+public:
+	VOnvifDisMgr();
+	~VOnvifDisMgr();
+public:
+private:
+
+};
+
+#endif /* __V_ONVIF_DIS_MGR_HPP__ */
