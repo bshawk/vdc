5228eea6f23a5d3c906f8bd39cb722ccab5c1a3c
xsmart <xsmart@163.com>
2014-08-16 18:10:40 +0800

    add rtsp server

---
+++ veuilib/include/vrtspserver.hpp
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// File: vrtspserver.hpp
+//
+// Desc: rtsp server - RTSP server.
+//
+// Copyright (c) 2014-2018 vdceye. All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef __V_RTSP_SERVER_HPP__
+#define __V_RTSP_SERVER_HPP__
+#include "utility.hpp"
+#include "debug.hpp"
+#include "videotype.hpp"
+#include <QWidget>
+#include "factory.hpp"
+#include <QThread>
+
+using namespace UtilityLib;
+
+class VE_LIBRARY_API VRTSPServer
+{
+public:
+	VRTSPServer();
+	~VRTSPServer();
+public:
+private:
+
+};
+
+#endif /* __V_RTSP_SERVER_HPP__ */
