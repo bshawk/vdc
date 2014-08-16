024a816e31894cb1729dbb3b43e3109df8c74ee5
xsmart <xsmart@163.com>
2014-08-16 18:12:22 +0800

    add virtual IPC manager

---
+++ veuilib/include/vvipcmgr.hpp
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// File: vvipcmgr.hpp
+//
+// Desc: virtual IPC manager - Manage virtual IP Camera.
+//
+// Copyright (c) 2014-2018 vdceye. All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef __V_V_IPC_MGR_HPP__
+#define __V_V_IPC_MGR_HPP__
+#include "utility.hpp"
+#include "debug.hpp"
+#include "videotype.hpp"
+#include <QWidget>
+#include "factory.hpp"
+#include <QThread>
+
+using namespace UtilityLib;
+
+class VE_LIBRARY_API VVIPCMgr
+{
+public:
+	VVIPCMgr();
+	~VVIPCMgr();
+public:
+private:
+
+};
+
+#endif /* __V_V_IPC_MGR_HPP__ */
