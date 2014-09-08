c97318114b5e66accbc8563ac6728c4dca06b257
xsmart <xsmart@163.com>
2014-09-08 10:09:17 +0800

    add video info type

--- velib/include/utility/videotype.hpp
+++ velib/include/utility/videotype.hpp
@@ -6,9 +6,21 @@ typedef enum __VideoStreamType
 {
 	VIDEO_STREAM_VIDEO = 1,
 	VIDEO_STREAM_AUDIO,
+	VIDEO_STREAM_INFO,
 	VIDEO_STREAM_TEXT,
 	VIDEO_STREAM_LAST
 }VideoStreamType;
+
+typedef enum __CodecType
+{
+        CODEC_H264 = 1,
+        CODEC_H265,
+        CODEC_MPEG4,
+        CODEC_MJPEG,
+        CODEC_PCMA,
+        CODEC_PCMU, 
+        CODEC_LAST
+}CodecType;
 
 typedef enum __VideoFrameType
 {
@@ -16,6 +28,7 @@ typedef enum __VideoFrameType
     VIDEO_FRM_I = 1,
     VIDEO_FRM_P = 2,
     VIDEO_FRM_B = 3,
+    VIDEO_FRM_INFO = 4,
     VIDEO_FRM_LAST
 } VideoFrameType;
 
@@ -28,6 +41,28 @@ typedef struct __VideoFrame
 	u32 dataLen;
 	u8   *dataBuf;
 }VideoFrame;
+
+typedef struct __InfoFrame
+{
+        CodecType video;
+        u32 vWidth;
+        u32 vHeight;
+        u32 vFps;
+        CodecType audio;
+        u32 aRate;
+        u32 aBitrate;
+        u32 aChannels;
+        u32 aBitspersample;
+        u32 padding1;
+        u32 padding2;
+        u32 padding3;
+        u32 padding4;
+        u32 padding5;
+        u32 padding6;
+        u32 padding7;
+        u32 padding8;
+}InfoFrame;
+
 
 #define MF_FILE_MAX_LENGTH 16 * 1024 * 1024
 
